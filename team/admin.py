3895f4b80f8696098d5fb3cf717c654e00f8818c
Harsh Vartak <vartak.harsh@gmail.com>
2020-01-30 12:59:34 +0530

    team setup

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Core_committee
 
-# Register your models here.
+admin.site.register(Core_committee)
--- team/models.py
+++ team/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Core_committee(models.Model):
+    name=models.CharField(max_length=255)
+    team=models.CharField(max_length=255, null=False)
+    pic=models.ImageField(upload_to="images/team")
+    position=models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)
