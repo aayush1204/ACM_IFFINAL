0de929ced04afc660695690b6c0d14d766102263
Harsh Vartak <vartak.harsh@gmail.com>
2020-01-30 12:55:40 +0530

    Added events

--- ACM_backend/urls.py
+++ ACM_backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # path('events/', include('events.urls')),
+    # path('if/', include('if.urls')),
+    path('loc/', include('loc.urls')),
+    # path('team/', include('team.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
